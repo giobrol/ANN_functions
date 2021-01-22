ee899038a249f9a3e5381c9bb3d6f1fe1f6dd2d6
giobrol <75173242+giobrol@users.noreply.github.com>
2021-01-21 21:36:34 -0300

    Update G12_M_ANN.m

--- MATLAB/G12_M_ANN.m
+++ MATLAB/G12_M_ANN.m
@@ -14,7 +14,7 @@ function [G12, G12HT] = G12_M_ANN(Vf, Gf, Gm)
 % HT - Halpin-Tsai model
 
 % For more information see this webpage:
-% COLOCAR LINK!
+% https://github.com/giobrol/ANN_functions
 
 if Gf>35
     warndlg('The value of Gf must be less than 35','Warning!');
